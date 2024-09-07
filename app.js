8a55005c2bd46b5bf0ef9bbc18f95cef0ac99d22
Gabriel Duarte <gabriel.anpedu@gmail.com>
2024-09-07 01:38:42 -0300

    Finalizando o projeto com GitHub e Deploy

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
 function pesquisar() {
-    let section = document.getElementById("resultados-pesquisa")
-    console.log(section);
+    let section = document.getElementById("resultados-pesquisa");
+    //console.log(section);
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value ;
+    // se campoPesquisa for uma string sem nada
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Nada foi encontrado! Você precisa digitar o nome de um atleta ou esporte</p>";
+        return
+    } 
     
-    let resultados = ""
+    campoPesquisa = campoPesquisa.toLowerCase();
+
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
     
     for (let dado of dados){
-        resultados += `
-        <div class="item-resultado">
-             <h2>
-              <a href="#" target="_blank">${dado.titulo}</a>
-             </h2>
-            <p class="descricao-meta">${dado.descricao}</p>
-            <a href=${dado.link} target="_blank">Mais Informações</a>
-        </div>
-    `
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLocaleLowerCase()
+        if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+            // cria novo elemento
+            resultados += `
+            <div class="item-resultado">
+                 <h2>
+                  <a href="#" target="_blank">${dado.titulo}</a>
+                 </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href=${dado.link} target="_blank">Mais Informações</a>
+            </div>
+        `
+        }
+       
+    }
+
+    if (!resultados) {
+        resultados = "<p>Nada encontrado</p>"
     }
     
     section.innerHTML = resultados
--- dados.js
+++ dados.js
@@ -2,16 +2,19 @@ let dados = [
     {
         titulo: "Rayssa Leal",
         descricao: "Rayssa Leal, a Fadinha, é um fenômeno do skate mundial. Com apenas [idade da Rayssa], ela já conquistou diversos títulos e se tornou um ícone da juventude. Sua habilidade e carisma a tornaram uma das atletas mais populares do Brasil.Z",
-        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
     },
     {
       titulo: "Darlan Romani",
       descricao: "Darlan Romani é um dos principais nomes do arremesso de peso brasileiro. Com seus recordes e participações em competições internacionais, ele representa o atletismo nacional com grande destaque.",
-      link: "https://pt.wikipedia.org/wiki/Darlan_Romani"
+      link: "https://pt.wikipedia.org/wiki/Darlan_Romani",
+      tags: "peso arremesso"
     },
     {
       titulo: "Rebeca Andrade",
       descricao: "Rebeca Andrade é, sem dúvida, um dos maiores nomes da ginástica artística brasileira e mundial. Com sua força, agilidade e determinação, ela conquistou o coração dos brasileiros e se tornou um símbolo de superação e sucesso. Suas medalhas olímpicas e mundiais, além de seus feitos históricos, a colocaram em um patamar de destaque no esporte.",
-      link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+      link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+      tags: "ginastica ouro"
     }
 ]
