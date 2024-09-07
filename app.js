7d36378c020e682fc9f2d8cee49637143cabd58d
Gabriel Duarte <gabriel.anpedu@gmail.com>
2024-09-07 01:02:24 -0300

    Implementando a funcionalidade de busca

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log(dados)
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa")
+    console.log(section);
+    
+    let resultados = ""
+    
+    for (let dado of dados){
+        resultados += `
+        <div class="item-resultado">
+             <h2>
+              <a href="#" target="_blank">${dado.titulo}</a>
+             </h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href=${dado.link} target="_blank">Mais Informações</a>
+        </div>
+    `
+    }
+    
+    section.innerHTML = resultados
+}
+
+
+
+
+
